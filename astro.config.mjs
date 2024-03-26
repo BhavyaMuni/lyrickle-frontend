af927ce4e120762a002ee4455e5c450449813d39
Bhavya Muni <bhavyamuni@outlook.com>
2024-03-26 14:26:53 -0400

    [🐛]: fixed vercel analytics

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,14 @@
 import { defineConfig } from "astro/config";
 import tailwind from "@astrojs/tailwind";
-
 import react from "@astrojs/react";
+
+import vercel from "@astrojs/vercel/serverless";
 
 // https://astro.build/config
 export default defineConfig({
-  devToolbar: { enabled: false },
+  devToolbar: {
+    enabled: false,
+  },
   integrations: [
     tailwind({
       applyBaseStyles: false,
@@ -14,7 +17,5 @@ export default defineConfig({
     react(),
   ],
   output: "server",
-  //   adapter: vercel({
-  //     webAnalytics: { enabled: true },
-  //   }),
+  adapter: vercel({ webAnalytics: { enabled: true } }),
 });
